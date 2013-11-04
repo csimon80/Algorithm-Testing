496c9b080d5a3776375bd0dd220bf496d49e107a
csimon80 <simon.chris.a@gmail.com>
2013-11-04 09:30:07 -0500

    updated IRLS, it know works!!!

--- LogisticRegression/IRLS_logreg.r
+++ LogisticRegression/IRLS_logreg.r
@@ -1,24 +1,32 @@
 rm(list=ls())
-#logistics data
-dat = array(dim = 81)
-dat[1]=81
-x = seq(-3,3,.151)
-dat[2:41]  = (sign(x+rnorm(40,0,.5))+1)/2
-dat[42:81] = x
-n=2
-fr <- function(beta) {
-  ll=0
-  for(i in 1:40){
-   z = beta[1] + beta[2]*dat[i+41]
-   #ll=ll - ( dat[i+1]*z - log( exp ( z ) + 1))
-   ll = ll - (dat[i+1] * log (1/(exp(-z) +1)) + (1-dat[i+1])*log (1 - 1/(exp(-z) +1)))
-  }
-  return(ll);
+#Challenger Data Set
+x = c(53,57,58,63,66,67,67,67,68,69,70,70,70,70,72,73,75,75,76,76,78,79,81)
+y = c( 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0)
+
+lr.solve = function(x,y=NULL){
+	x.mat=as.matrix(x)
+	n=dim(x.mat)
+	m=n[1] ; n=n[2]
+	if(!is.null(y)){
+	  X=matrix(c(rep(1,m),x.mat),ncol=n+1)
+		Y = matrix(y,m,1)
+		beta=matrix(rep(0,n),n,1)
+		b=matrix(rep(10,n),n,1)
+	}
+	else{
+	  X=matrix(c(rep(1,m),x.mat[,-n]),ncol=n)
+		Y = x.mat[,n]
+		beta=matrix(rep(0,n),n,1)
+		b=matrix(rep(10,n),n,1)
+	}
+	while(norm(matrix(beta-b))>1e-5){
+		b=beta
+		p=1/(1+exp(-X%*%b))
+		G=t(X)%*%diag(-c(p*(1-p)))%*%X
+		beta=b-solve(G,t(X)%*%(Y-p))
+	}
+	return(beta)
 }
-beta=array(dim=2)
-beta[1]=0
-beta.seq = seq(0,4,.1)
-ll = array(dim=length(beta.seq))
-for(i in 1:length(beta.seq)){
-  ll[i]=fr(c(beta[1],beta.seq[i]))
-}
+iris.y=iris
+iris.y$Species = as.numeric((iris$Species=='virginica'))
+lr.solve(iris.y)
